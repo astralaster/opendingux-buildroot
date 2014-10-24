ced68ab9224979ebbb80d4e88ea99276f90bc50d
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2014-10-24 09:35:33 +0400

    squashfs: Fix download link

--- package/squashfs/squashfs.mk
+++ package/squashfs/squashfs.mk
@@ -1,6 +1,6 @@
 SQUASHFS_VERSION=4.2
 SQUASHFS_SOURCE=squashfs$(SQUASHFS_VERSION).tar.gz
-SQUASHFS_SITE=http://downloads.sourceforge.net/projct/squashfs/squashfs/squashfs$(SQUASHFS_VERSION)
+SQUASHFS_SITE=http://downloads.sourceforge.net/project/squashfs/squashfs/squashfs$(SQUASHFS_VERSION)
 
 # no libattr in BR
 SQUASHFS_MAKE_ARGS = XATTR_SUPPORT=0
