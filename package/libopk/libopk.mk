0fa1e48533d59d824f0ecc3ee219644137b62fd8
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2015-04-24 17:21:25 +0300

    libopk: Fix compilation and installation
    Previously opkinfo and opkrun were not being copied to rootfs

--- package/libopk/libopk.mk
+++ package/libopk/libopk.mk
@@ -11,6 +11,19 @@ LIBOPK_INSTALL_STAGING = YES
 LIBOPK_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" \
 				  CROSS_COMPILE="$(TARGET_CROSS)" PREFIX=/usr
 
+define LIBOPK_BUILD_PYTHON
+	(cd $(@D)/python ; $(HOST_DIR)/usr/bin/python setup.py build)
+endef
+define LIBOPK_INSTALL_PYTHON
+	(cd $(@D)/python ; $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+endef
+
+ifeq ($(BR2_PACKAGE_PYTHON),y)
+	LIBOPK_DEPENDENCIES += python host-python
+	LIBOPK_POST_BUILD_HOOKS += LIBOPK_BUILD_PYTHON
+	LIBOPK_POST_INSTALL_TARGET_HOOKS += LIBOPK_INSTALL_PYTHON
+endif
+
 define LIBOPK_BUILD_CMDS
 	$(LIBOPK_MAKE_ENV) $(MAKE) -C $(@D)
 endef
@@ -20,7 +33,7 @@ define LIBOPK_INSTALL_STAGING_CMDS
 endef
 
 define LIBOPK_INSTALL_TARGET_CMDS
-	$(LIBOPK_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) install-lib
+	$(LIBOPK_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) install-bin install-lib
 endef
 
 $(eval $(generic-package))
